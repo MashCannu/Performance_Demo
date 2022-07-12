c8990108809262076fded6a8c59f2e1cada05c67
MashCannu <mash.cannu@gmail.com>
2022-07-13 03:21:50 +0900

    Create iPerf3 demo script

---
+++ iPerf3_Demo/ClientSide/start_iPerf3_client.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001　＞ NUL
+
+start /I /MIN powershell -ExecutionPolicy RemoteSigned -File ./iPerf3_clnt_1.ps1
+
+start /I /MIN powershell -ExecutionPolicy RemoteSigned -File ./iPerf3_clnt_2.ps1
+
+start /MAX C:\"Program Files\tcp monitor"\tcpmon.exe
+
+exit
